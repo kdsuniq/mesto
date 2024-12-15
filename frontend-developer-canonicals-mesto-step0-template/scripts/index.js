b6d916edf46c97831df3e67fa6bb243dd3e0a74e
Daria Komarova <kds.uniq@mail.ru>
2024-12-16 00:22:45 +0500

    js

--- frontend-developer-canonicals-mesto-step0-template/scripts/index.js
+++ frontend-developer-canonicals-mesto-step0-template/scripts/index.js
@@ -1,9 +1,42 @@
-// @todo: Темплейт карточки
+const profilePopup = document.querySelector('popup_type_edit');
+const cardPopup = document.querySelector(".popup_type_new-card");
+const imagePopup = document.querySelector(".popup_type_image");
+const profileFormElement = profilePopup.querySelector(".popup__form");
+const nameInput = document.querySelector(".popup__input_type_name");
+const jobInput = document.querySelector(".popup__input_type_description");
 
-// @todo: DOM узлы
+profilePopup.classList.add("popup_is-animated");
+cardPopup.classList.add("popup_is-animated");
+imagePopup.classList.add("popup_is-animated");
 
-// @todo: Функция создания карточки
+function openModal(popup) {      
+    popup.classList.add('popup_is-opened');
+}
 
-// @todo: Функция удаления карточки
+function closeModal(popup) {      
+    popup.classList.toggle('popup_is-opened');
+}
 
-// @todo: Вывести карточки на страницу
+const profileButton = document.querySelector(".profile__edit-button");
+const closeProfilePopupButton = profilePopup.querySelector(".popup__close");
+
+profileButton.addEventListener('click', function(){
+    nameInput.value = document.querySelector(".profile__title").textContent;
+    jobInput.value = document.querySelector(".profile__description").textContent;
+    openModal(profilePopup);
+});
+
+closeProfilePopupButton.addEventListener('click', function(){
+    closeModal(profilePopup);
+});
+
+// Обработчик «отправки» формы, хотя пока
+// она никуда отправляться не будет
+function handleProfileFormSubmit(evt) {
+    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+
+    document.querySelector(".profile__title").textContent = nameInput.value;
+    document.querySelector(".profile__description").textContent = jobInput.value;
+    closeModal(profilePopup);
+}
+profileFormElement.addEventListener('submit', handleProfileFormSubmit);
